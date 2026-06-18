ba9f9641a5693fb45d1257f9bca221ca6887ceb0
Theodoros Nicolaou <theonicolaoucostas@gmail.com>
2026-06-18 20:28:04 +0300

    http: get_status_code_phrase changed to use case-switch

--- src/libstrtb/http/protocol.cpp
+++ src/libstrtb/http/protocol.cpp
@@ -7,56 +7,59 @@
 
 namespace strtb::http {
 
-static std::map<unsigned int, const char*> status_code_phrases = {
-    {100, "Continue"},
-    {101, "Switching Protocols"},
-    {200, "OK"},
-    {201, "Created"},
-    {202, "Accepted"},
-    {203, "Non-Authoritative Information"},
-    {204, "No Content"},
-    {205, "Reset Content"},
-    {206, "Partial Content"},
-    {300, "Multiple Choices"},
-    {301, "Moved Permamently"},
-    {302, "Found"},
-    {303, "See Other"},
-    {304, "Not Modified"},
-    {305, "Use Proxy"},
-    {307, "Temporary Redirect"},
-    {308, "Permanent Redirect"},
-    {400, "Bad Request"},
-    {401, "Unauthorized"},
-    {402, "Payment Required"},
-    {403, "Forbidden"},
-    {404, "Not Found"},
-    {405, "Method Not Allowed"},
-    {406, "Not Acceptable"},
-    {407, "Proxy Authentication Required"},
-    {408, "Request Timeout"},
-    {409, "Conflict"},
-    {410, "Gone"},
-    {411, "Length Required"},
-    {412, "Precondition Failed"},
-    {413, "Content Too Large"},
-    {414, "URI Too Long"},
-    {415, "Unsupported Media Type"},
-    {416, "Range Not Satisfiable"},
-    {417, "Expectation Failed"},
-    {418, "I'm a teapot"},
-    {421, "Misdirected Request"},
-    {422, "Unprocessable Content"},
-    {426, "Upgrade Required"},
-    {428, "Precondition Required"},
-    {429, "Too Many Requests"},
-    {431, "Request header Fields Too Large"},
-    {500, "Internal Server Error"},
-    {501, "Not Implemented"},
-    {502, "Bad Gateway"},
-    {503, "Service Unavailable"},
-    {504, "Gateway Timeout"},
-    {505, "HTTP Version Not Supported"},
-    {511, "Network Authentication Required"}
+const char* get_status_code_phrase(int status_code) {
+    switch (status_code) {
+    case 100: return "Continue";
+    case 101: return "Switching Protocols";
+    case 200: return "OK";
+    case 201: return "Created";
+    case 202: return "Accepted";
+    case 203: return "Non-Authoritative Information";
+    case 204: return "No Content";
+    case 205: return "Reset Content";
+    case 206: return "Partial Content";
+    case 300: return "Multiple Choices";
+    case 301: return "Moved Permamently";
+    case 302: return "Found";
+    case 303: return "See Other";
+    case 304: return "Not Modified";
+    case 305: return "Use Proxy";
+    case 307: return "Temporary Redirect";
+    case 308: return "Permanent Redirect";
+    case 400: return "Bad Request";
+    case 401: return "Unauthorized";
+    case 402: return "Payment Required";
+    case 403: return "Forbidden";
+    case 404: return "Not Found";
+    case 405: return "Method Not Allowed";
+    case 406: return "Not Acceptable";
+    case 407: return "Proxy Authentication Required";
+    case 408: return "Request Timeout";
+    case 409: return "Conflict";
+    case 410: return "Gone";
+    case 411: return "Length Required";
+    case 412: return "Precondition Failed";
+    case 413: return "Content Too Large";
+    case 414: return "URI Too Long";
+    case 415: return "Unsupported Media Type";
+    case 416: return "Range Not Satisfiable";
+    case 417: return "Expectation Failed";
+    case 418: return "I'm a teapot";
+    case 421: return "Misdirected Request";
+    case 422: return "Unprocessable Content";
+    case 426: return "Upgrade Required";
+    case 428: return "Precondition Required";
+    case 429: return "Too Many Requests";
+    case 431: return "Request header Fields Too Large";
+    case 500: return "Internal Server Error";
+    case 501: return "Not Implemented";
+    case 502: return "Bad Gateway";
+    case 503: return "Service Unavailable";
+    case 504: return "Gateway Timeout";
+    case 505: return "HTTP Version Not Supported";
+    case 511: return "Network Authentication Required";
+    default : return "";    // empty phrase for unknown codes
+    }
 };
 
 typedef enum month_enum {
@@ -517,14 +520,6 @@ void field_parser::clear() {
     fields.clear();
     fields_set_cookie.clear();
     _previous_field.clear();
-}
-
-const char* get_status_code_phrase(int status_code) {
-    auto phrase = status_code_phrases.find(status_code);
-    if (phrase == status_code_phrases.end())
-        return "";  // empty phrase for unknown codes
-    else
-        return phrase->second;
 }
 
 static constexpr size_t strlen_constexpr(const char *str) {
